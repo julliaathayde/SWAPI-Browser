27c6b412f3c87192c5562403f83ed8df9a66f5ba
Jullia Athayde <julliapatricia@gmail.com>
2019-03-18 20:23:09 -0300

    Altera autor

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import React, { Component } from 'react';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-import Header from './components/Header';
-import MenuXs from './components/MenuXs';
-import MainMenu from './components/MainMenu';
-import Home from './components/Home'
-import CharacterContainer from './components/CharacterContainer';
-import PeopleContainer from './components/PeopleContainer';
-import PlanetsContainer from './components/PlanetsContainer';
-import PlanetContainer from './components/PlanetContainer';
-import VehiclesContainer from './components/VehiclesContainer';
-import VehicleContainer from './components/VehicleContainer';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Header from "./components/Header";
+import MenuXs from "./components/MenuXs";
+import MainMenu from "./components/MainMenu";
+import Home from "./components/Home";
+import CharacterContainer from "./components/CharacterContainer";
+import PeopleContainer from "./components/PeopleContainer";
+import PlanetsContainer from "./components/PlanetsContainer";
+import PlanetContainer from "./components/PlanetContainer";
+import VehiclesContainer from "./components/VehiclesContainer";
+import VehicleContainer from "./components/VehicleContainer";
+import "./App.css";
 
 class App extends Component {
   render() {
@@ -23,29 +23,23 @@ class App extends Component {
             <MenuXs />
             <hr />
             <Switch>
-              <Route exact path='/'
-                component={Home} />
+              <Route exact path="/" component={Home} />
 
-              <Route exact path='/people'
-                component={PeopleContainer} />
-              <Route exact path='/people/:id'
-                component={CharacterContainer}/>
+              <Route exact path="/people" component={PeopleContainer} />
+              <Route exact path="/people/:id" component={CharacterContainer} />
 
-              <Route exact path='/planets'
-                component={PlanetsContainer} />
-              <Route exact path='/planets/:id'
-                component={PlanetContainer} />
-                
-              <Route exact path='/vehicles'
-                component={VehiclesContainer} />
-              <Route exact path='/vehicles/:id/'
-                component={VehicleContainer} />
+              <Route exact path="/planets" component={PlanetsContainer} />
+              <Route exact path="/planets/:id" component={PlanetContainer} />
+
+              <Route exact path="/vehicles" component={VehiclesContainer} />
+              <Route exact path="/vehicles/:id/" component={VehicleContainer} />
             </Switch>
             <footer className="footer">
               <hr />
               <p className="text-center">
                 <small>
-                  2019 Jullia Athayde. Made with <i className="fas fa-heart"></i> in Fortaleza, Brazil
+                  2019 Jullia Chaves. Made with <i className="fas fa-heart" />{" "}
+                  in Fortaleza, Brazil
                 </small>
               </p>
             </footer>
